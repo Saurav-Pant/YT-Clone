28066bb65307ac98e365a14310009bca708afd31
Saurav Pant <pantsaurav005@gmail.com>
2023-01-23 20:01:04 +0530

    Not Much But Ok

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const ChannelDetail = () => {
   return (
-    <div>ChannelDetail</div>
+    <div>
+      
+    </div>
   )
 }
 
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,14 +1,10 @@
-import React,{useState,useEffect}from 'react'
-import {Box,Stack,Typography} from '@mui/material'
+import React from 'react'
 
 const Feed = () => {
   return (
-    <Stack sx={{flexDirection:{sx:"column",md:"row"}}}>
-       <Box sx={{height :{sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: {sx : 0, md: 2}}}>
-
-      </Box>
-
-    </Stack>
+    <div>
+      
+    </div>
   )
 }
 
