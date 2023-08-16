8b849a1a254b1ba8c2146f4f1344d2145ef82049
Saurav Pant <pantsaurav005@gmail.com>
2023-08-16 11:52:11 +0530

    Logo Link Corrected

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import {Stack} from '@mui/material'
-import { Link } from '@mui/material';
 import {logo} from '../utils/constants'
 import SearchBar from './SearchBar'
+import { Link } from 'react-router-dom';
 
 
 const Navbar = () => {
